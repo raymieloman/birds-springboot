69bfcc9f204fb09b0bd191b9c1242a20feeaa462
Raymond Loman <raymond@carpago.nl>
2023-10-17 11:25:08 +0200

    change from crudrepo to jparepo

--- src/main/java/nl/youngcapital/birds/persistence/BirdRepository.java
+++ src/main/java/nl/youngcapital/birds/persistence/BirdRepository.java
@@ -1,12 +1,12 @@
 package nl.youngcapital.birds.persistence;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import nl.youngcapital.birds.model.Bird;
 
 
 @Repository
-public interface BirdRepository extends CrudRepository<Bird, Long> {
+public interface BirdRepository extends JpaRepository<Bird, Long> {
 
 }
--- src/main/java/nl/youngcapital/birds/service/BirdService.java
+++ src/main/java/nl/youngcapital/birds/service/BirdService.java
@@ -1,5 +1,7 @@
 package nl.youngcapital.birds.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,7 @@ public class BirdService {
 	private BirdRepository birdRepository;
 	
 	
-	public Iterable<Bird> findAll() {
+	public List<Bird> findAll() {
 		
 		return birdRepository.findAll();
 	}
