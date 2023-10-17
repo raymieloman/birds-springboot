38e77b0ca02162de489a1c7fe3a7d60fdee5da9e
Raymond Loman <raymond@carpago.nl>
2023-10-17 09:50:45 +0200

    Initial load

---
+++ src/test/java/nl/youngcapital/birds/BirdsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.youngcapital.birds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
